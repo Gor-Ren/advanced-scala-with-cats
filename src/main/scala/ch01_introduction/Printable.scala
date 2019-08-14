d652fd2a4d44d8ea6a1859f9a2637228368dafdb
Gor-Ren <gor.rennie@gmail.com>
2019-08-14 09:05:21 +0100

    Complete chap1.3

--- src/main/scala/ch01_introduction/Printable.scala
+++ src/main/scala/ch01_introduction/Printable.scala
@@ -8,12 +8,6 @@ object PrintableInstances {
   implicit val printableString: Printable[String] = (value: String) => value
 
   implicit val printableInt: Printable[Int] = (value: Int) => value.toString
-
-  implicit val printableCat: Printable[Cat] = new Printable[Cat] {
-    override def format(cat: Cat): String =
-      Printable.format(cat.name) + "is a " + Printable.format(cat.age) +
-        "year-old " + Printable.format(cat.colour) + " cat."
-  }
 }
 
 object Printable {
@@ -26,3 +20,36 @@ object Printable {
 }
 
 final case class Cat(name: String, age: Int, colour: String)
+
+// decided to place the type class instance in the companion object
+object Cat {
+  import PrintableInstances.{printableInt, printableString}
+
+  implicit val printableCat: Printable[Cat] = (cat: Cat) =>
+    s"${Printable.format(cat.name)} is a " +
+      s"${Printable.format(cat.age)} year-old " +
+      s"${Printable.format(cat.colour)} cat."
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+
+    def format(implicit p: Printable[A]): String =
+      p.format(value)
+
+    def print()(implicit p: Printable[A]): Unit =
+      println(format)
+  }
+}
+
+object PrintableExercise extends App {
+  /* First Exercise: Print a cat */
+  val cat: Cat = Cat("Nina", 4, "black")
+
+  import PrintableInstances._
+  Printable.print(cat)
+
+  /* Second Exercise: Print cat using extension method (prettier syntax) */
+  import PrintableSyntax._
+  cat.print()
+}
