388502fd80f21e4ec971990342468b343d6568b4
Gor-Ren <gor.rennie@gmail.com>
2019-11-24 11:32:48 +0000

    Complete chapter 5 exercise, Monad Transformers

--- src/main/scala/ch05_monad_transformers/MonadTExercise.scala
+++ src/main/scala/ch05_monad_transformers/MonadTExercise.scala
@@ -3,8 +3,9 @@ package ch05_monad_transformers
 import cats.data.EitherT
 import cats.instances.future._
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.ExecutionContext.global
+import scala.concurrent.duration._
 
 class MonadT {
   implicit val ec: ExecutionContext = global
@@ -12,17 +13,32 @@ class MonadT {
   // a response is a fallible async call
   type Response[A] = EitherT[Future, String, A]
 
-  def getPowerLevel(autobot: String): Response[Int] =
+  def getPowerLevel(ally: String): Response[Int] =
     EitherT(
       Future(
         PowerData.powerLevels
-          .get(autobot)
-          .toRight(s"No power level response from autobot: $autobot")
+          .get(ally)
+          .toRight(s"No power level response from autobot: $ally")
       )
     )
+
+  def canSpecialMove(ally1: String, ally2: String): Response[Boolean] =
+    for {
+      power1 <- getPowerLevel(ally1)
+      power2 <- getPowerLevel(ally2)
+    } yield power1 + power2 >= PowerData.SPECIAL_MOVE_POWER_REQ
+
+  def tacticalReport(ally1: String, ally2: String): String =
+    Await.result(canSpecialMove(ally1, ally2).value, 1 second) match {
+      case Left(msg)    => msg
+      case Right(false) => s"$ally1 and $ally2 cannot perform a special move"
+      case Right(true)  => s"$ally1 and $ally2 can perform a special move"
+    }
 }
 
 object PowerData {
+
+  val SPECIAL_MOVE_POWER_REQ: Int = 15
 
   val powerLevels: Map[String, Int] = Map(
     "Jazz" -> 6,
